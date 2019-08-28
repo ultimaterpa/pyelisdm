5f9d04a37afe8eb593865cd1c39890247ca33096
Slavaqq <slavaqq@gmail.com>
2019-08-28 12:36:24 +0200

    base_url support

--- pyelisdm/pyelisdm.py
+++ pyelisdm/pyelisdm.py
@@ -1,29 +1,28 @@
 import requests
+from urllib.parse import urljoin
 
-URL = "https://api.elis.rossum.ai/v1"
+
+URL = "https://api.elis.rossum.ai"
 CONTENT_TYPE = {"Content-Type": "application/json"}
 
 
 class Elis:
-    def __init__(self, username, password, url=URL, max_token_lifetime_s=None):
+    def __init__(self, username, password, base_url=URL, max_token_lifetime_s=None):
         self.username = username
         self.password = password
-        self.url = url
         self.max_token_lifetime_s = max_token_lifetime_s
-        self._session = requests.Session()
-        self._authorization = None
+        self._session = APISession(base_url=base_url)
 
     def __enter__(self):
         self.login()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self._authorization:
-            self.logout()
+        self.logout()
 
     def login(self):
         response = self._session.post(
-            f"{URL}/auth/login",
+            "v1/auth/login",
             headers=CONTENT_TYPE,
             json={"username": self.username, "password": self.password},
         )
@@ -31,16 +30,16 @@ class Elis:
         self._session.headers.update({"Authorization": f"token {response.json()['key']}"})
 
     def logout(self):
-        response = self._session.post(f"{URL}/auth/logout")
+        response = self._session.post("v1/auth/logout")
         response.raise_for_status()
 
     def queues(self, ordering=None):
-        response = self._session.get(f"{URL}/queues", params={"ordering": ordering})
+        response = self._session.get("v1/queues", params={"ordering": ordering})
         response.raise_for_status()
         return response.json()
 
     def queue(self, queue_id):
-        response = self._session.get(f"{URL}/queues/{queue_id}")
+        response = self._session.get(f"/queues/{queue_id}")
         response.raise_for_status()
         return Queue(response.json())
 
@@ -59,3 +58,13 @@ class Queue:
 
 class Schema:
     pass
+
+
+class APISession(requests.Session):
+    def __init__(self, base_url):
+        self.base_url = base_url
+        super().__init__()
+
+    def request(self, method, url, *args, **kwargs):
+        url = urljoin(self.base_url, url)
+        return super().request(method, url, *args, **kwargs)
