ef0ad5a38acf9df93d0de010fa53253379a2c614
Slavaqq <slavaqq@gmail.com>
2019-09-03 07:56:12 +0200

    add Queue.upload method

--- pyelisdm/pyelisdm.py
+++ pyelisdm/pyelisdm.py
@@ -11,46 +11,50 @@ class Elis:
         self.username = username
         self.password = password
         self.max_token_lifetime_s = max_token_lifetime_s
-        self._session = URLBaseSession(base_url=base_url)
+        self.session = URLBaseSession(base_url=base_url)
 
     def __enter__(self):
-        self._login()
+        self.login()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._logout()
+        self.logout()
 
-    def _login(self):
-        response = self._session.post(
+    def login(self):
+        response = self.session.post(
             "v1/auth/login",
             headers=CONTENT_TYPE,
             json={"username": self.username, "password": self.password},
         )
         response.raise_for_status()
-        self._session.headers.update({"Authorization": f"token {response.json()['key']}"})
+        self.session.headers.update({"Authorization": f"token {response.json()['key']}"})
 
-    def _logout(self):
-        response = self._session.post("v1/auth/logout")
+    def logout(self):
+        response = self.session.post("v1/auth/logout")
         response.raise_for_status()
 
     def queues(self, ordering=None):
-        response = self._session.get("v1/queues", params={"ordering": ordering})
+        response = self.session.get("v1/queues", params={"ordering": ordering})
         response.raise_for_status()
         return response.json()
 
     def queue(self, queue_id):
-        response = self._session.get(f"v1/queues/{queue_id}")
+        response = self.session.get(f"v1/queues/{queue_id}")
         response.raise_for_status()
-        return Queue(response.json())
+        return Queue(self.session, response.json())
 
 
 class Queue:
-    def __init__(self, response):
+    def __init__(self, session, response):
+        self.session = session
         self.id = response["id"]
         self.url = response["url"]
 
-    def upload(self):
-        pass
+    def upload(self, invoice, values=None):
+        files = {"content": open(invoice, "rb")}
+        response = self.session.post(f"v1/queues/{self.id}/upload", files=files, data=values)
+        print(response.text)
+        response.raise_for_status()
 
     def export(self):
         pass
